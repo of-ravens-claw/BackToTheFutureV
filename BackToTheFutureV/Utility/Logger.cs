902acb23a5c32cdc2a2a1dd4d488e816f53d4a89
of-ravens-claw <victorgamer0720yt@gmail.com>
2023-01-14 01:18:46 -0300

    Update Logger.cs
    Adds a license.

--- BackToTheFutureV/Utility/Logger.cs
+++ BackToTheFutureV/Utility/Logger.cs
@@ -1,5 +1,29 @@
+/* GTA V C# Logging Script, inspired by ranstar74's rageAm Logger.
+ * 
+ * MIT License
+ * 
+ * Copyright (c) 2023 of-ravens-claw
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+*/
+
 using System;
-using System.Globalization;
 using System.IO;
 
 namespace BackToTheFutureV
@@ -17,10 +41,6 @@ namespace BackToTheFutureV
 			if (File.Exists(LogLocation))
 				File.Move(LogLocation, OldLogLocation);
 		}
-		
-		public static void Log(string str) => LogI(str);
-		// Because someone will attempt to use Log instead of the proper functions.
-		// Moron proof software is always annoying.
 		
 		// Line Feed is the only one that matters :)
 		// To whoever is using an ancient system that cannot understand Line Feed, and instead relies on Carriage Return,
